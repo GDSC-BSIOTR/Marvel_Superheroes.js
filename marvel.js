033c2bf7c92b3faf63de8c007369e7074ffa9341
Priyanka Sonawane <61462728+Priyanka2119@users.noreply.github.com>
2022-10-01 00:04:24 +0530

    update info

--- marvel.js
+++ marvel.js
@@ -59,4 +59,11 @@
     "Actor Name": "Chadwick Aaron Boseman",
     "SuperPower": "Vibraniam Sute"
     }, 
+    {
+    "Character Name": "Hawkeye",
+    "Reel Name": "Clint Barton",
+    "Actor Name": "Jeremy Renner",
+    "SuperPower": "Goliath"
+    },
+    
   ]
