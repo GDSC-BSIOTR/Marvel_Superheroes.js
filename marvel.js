7cc581976be4b3e272b1b0f8be12469d7b5169a2
Janhavi Salunkhe <90143596+JanhaviS29@users.noreply.github.com>
2022-09-30 22:02:16 +0530

    updated info

--- marvel.js
+++ marvel.js
@@ -44,4 +44,10 @@
     "Actor Name": "Chadwick Aaron Boseman",
     "SuperPower": "Vibraniam Sute"
     }, 
+     {
+    "Character Name": "black widow",
+    "Reel Name": "black widow",
+    "Actor Name": "Natasha Romanoff",
+    "SuperPower": "covert arts of espionage,infiltration & subterfuge."
+    }, 
   ]
